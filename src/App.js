44701db7a2d04fde2faeb8d2e5930a61175b6373
julioczrn <julinhobiu8@gmail.com>
2023-06-05 09:32:50 -0300

    Projeto-React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+
+import People from './assets/people.svg';
+import Seta from './assets/Seta.svg';
+import Trash from './assets/Trash.svg';
+import axios from 'axios';
+
+import {
+  Container,
+  H1,
+  Image,
+  ContainerItens,
+  InputLabel,
+  Input,
+  Button,
+  User
+} from "./styles";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef()
+  const inputAge = useRef()
+
+  async function addNewuser() {
+    //setUsers([...users,{id: Math.random(), name: inputName.current.value, age: inputAge.current.value}])
+    const data = await axios.post("http://localhost:3001", {
+       name: inputName.current.value,
+        age: inputAge.current.value, 
+      })};
+
+  function deleteUser(userId) {
+    const newUsers = users.filter((user) => user.id !== userId);
+
+    setUsers(newUsers);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image alt="logo-imagem" src={People} />
+      <ContainerItens>
+
+        <H1>Ola!</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder='Nome' />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder='Idade' />
+
+        <Button onClick={addNewuser}>
+          Cadastrar
+          <img alt='Seta' src={Seta} />
+        </Button>
+
+
+        <ul>
+          {users.map(user => (
+            <User key={user.id}>
+              <p>{user.name}</p> - <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt='lata-de-lixo'></img></button>
+            </User>
+          ))}
+        </ul>
+      </ContainerItens>
+    </Container>
   );
 }
 
